9031674e5a3a2a6ac0a5a2ddec25db9e0b0bd4e7
Marlon <marlon.da.silva996@gmail.com>
2026-03-03 18:11:13 -0300

    feat(routes/order): implementando verificações para validar a requisição

--- routes/pedidos.py
+++ routes/pedidos.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from dependencies.dependencies import sessao, verificar_token
 from schemas import PedidoSchema
-from models.models import Pedido
+from models.models import Pedido, Usuario
 
 pedidos_router = APIRouter(prefix="/pedidos", tags=["pedidos"], dependencies=[Depends(verificar_token)])
 
@@ -18,12 +18,16 @@ async def criar_pedido(pedido_schema: PedidoSchema, session: Session = Depends(s
     return {"mensagem": "pedido criado com sucesso! id do pedido: {}".format(novo_pedido.id)}
 
 @pedidos_router.post("/pedido/cancelar/{id_pedido}")
-async def cancelar_pedido(id_pedido: int, session: Session = Depends(sessao)):
+async def cancelar_pedido(id_pedido: int, session: Session = Depends(sessao), usuario: Usuario = Depends(verificar_token)):
+    # se usuario.perfilAdmin = True e se usuario.id == pedido.usuario, são duas verificações que eu irei fazer para validar a requisição
     pedido = session.query(Pedido).filter(Pedido.id==id_pedido).first()
     if not pedido: 
-        raise HTTPException(status_code=400, detail="Pedido não encontrado!")  
+        raise HTTPException(status_code=400, detail="Pedido não encontrado!")
+    if not usuario.perfilAdmin and usuario.id != pedido.usuario:
+        raise HTTPException(status_code=401, detail="Você não tem autorização para fazer essa modificação!")
     pedido.status = "CANCELADO"
     session.commit()
     return {"mensagem": "Pedido #{} cancelado com sucesso!".format(id_pedido),
             "pedido": pedido
             }
+
