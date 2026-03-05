ac3f01e98b052e3eaf3ee54c993754c5db2aa90a
Marlon <marlon.da.silva996@gmail.com>
2026-03-04 21:12:22 -0300

    feat(routes/order): estrutura inicial da rota de adicionar item ao pedido

--- routes/pedidos.py
+++ routes/pedidos.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from dependencies.dependencies import sessao, verificar_token
-from schemas import PedidoSchema
-from models.models import Pedido, Usuario
+from schemas import PedidoSchema, ItemPedidoSchema
+from models.models import Pedido, Usuario, ItemPedido
 
 pedidos_router = APIRouter(prefix="/pedidos", tags=["pedidos"], dependencies=[Depends(verificar_token)])
 
@@ -42,3 +42,18 @@ async def listar_pedidos(session: Session = Depends(sessao), usuario: Usuario = 
             "pedidos": pedidos
         }
 
+@pedidos_router.post("/pedido/adicionar-item/{id_pedido}")
+async def adicionar_item_pedido(id_pedido: int,
+                                item_pedido_schema: ItemPedidoSchema, 
+                                session: Session = Depends(sessao), 
+                                usuario: Usuario = Depends(verificar_token)):
+    pedido = session.query(Pedido).filter(Pedido.id==id_pedido).first()
+    if not pedido:
+        raise HTTPException(status_code=400, detail="Pedido não existente!")
+    if not usuario.perfilAdmin and usuario.id != pedido.usuario:
+        raise HTTPException(status_code=401, detail="Você não tem autorização para fazer essa operação!")
+    item_pedido = ItemPedido(item_pedido_schema.quantidade,
+                             item_pedido_schema.sabor,
+                             item_pedido_schema.tamanho,
+                             item_pedido_schema.preco_unitario, id_pedido)
+    
